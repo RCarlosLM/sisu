4b829f2b3a7261c8a4b2545b53c06a39e24e2af2
Roberto Carlos <informore94@gmail.com>
2020-07-07 03:47:20 -0500

    quitando config

--- php/config.php
+++ php/config.php
@@ -1,9 +1,9 @@
 <?php  
 
-    $host = "localhost";
-    $username = "root";
-    $password = "12345678";
-    $DBname = "sisu";
+    $host = "";
+    $username = "";
+    $password = "";
+    $DBname = "";
     
     $con = mysqli_connect($host,$username,$password,$DBname);
     mysqli_set_charset($con,'utf8');
